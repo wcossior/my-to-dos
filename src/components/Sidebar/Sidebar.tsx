ec1bb65a424e635ebd2d0f1e4ff1c6a005715421
Wilder Cossio <w.cossio.r@gmail.com>
2024-01-15 19:43:07 -0400

    creating my to-dos project

---
+++ src/components/Sidebar/Sidebar.tsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Sidebar extends Component {
+  render() {
+    return (
+      <div>
+        Hola esto es un sidebar
+      </div>
+    )
+  }
+}
