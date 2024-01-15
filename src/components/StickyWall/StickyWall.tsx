344c2edda0c94890fa8ccd1f740fb5cc1944962d
Wilder Cossio <w.cossio.r@gmail.com>
2024-01-15 19:49:45 -0400

    creating the StickyWall component

---
+++ src/components/StickyWall/StickyWall.tsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class StickyWall extends Component {
+  render() {
+    return (
+      <div>
+        This is the task's wall
+      </div>
+    )
+  }
+}
