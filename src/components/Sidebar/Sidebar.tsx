30303f60f5c1ec03e61d1de04d6efce9aeaed783
Wilder Cossio <w.cossio.r@gmail.com>
2024-01-18 16:00:08 -0400

    styling the sidebar

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,11 +1,40 @@
 import React, { Component } from 'react';
+import logoutIcon from "../../assets/logout.svg";
+import userIcon from "../../assets/user.svg";
+import addIcon from "../../assets/add.svg";
 import "./Sidebar.css";
 
 export default class Sidebar extends Component {
   render() {
     return (
       <div className='sidebar'>
-        Hola esto es un sidebar
+        <div className='user-container'>
+          <img className='user-icon' src={userIcon} alt="user icon" />
+          <div className='info-user'>
+            <h2>Hi, Wilder!</h2>
+            <p>w.cossio.r@gmail.com</p>
+          </div>
+        </div>
+        <h1>Menu</h1>
+        <div className='groups'>
+          <div className='btn-add-a-group'>
+            <h3>Groups</h3>
+            <img className='add-a-group' src={addIcon} alt="add a group icon" />
+          </div>
+          <div className='title-group-container selected'>
+            <p>Ui Design</p>
+          </div>
+          <div className='title-group-container'>
+            <p>Grosery list</p>
+          </div>
+          <div className='title-group-container'>
+            <p>Vacation checklist</p>
+          </div>
+        </div>
+        <div className='sign-out-option'>
+          <img className='logout-icon' src={logoutIcon} alt="logout icon" />
+          <p>Sign out</p>
+        </div>
       </div>
     )
   }
--- src/components/StickyWall/StickyWall.tsx
+++ src/components/StickyWall/StickyWall.tsx
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import "./StickyWall.css";
+import logo from "../../assets/logo.svg";
 
 export default class StickyWall extends Component {
-  render() {
-    return (
-      <div className='stickywall'>
-        This is the task's wall
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div className='stickywall'>
+                <div className='title'>
+                    <img className='logo' src={logo} alt="logo" />
+                    <h1>To-dos</h1><h2>wall</h2>
+                </div>
+            </div>
+        )
+    }
 }
